4cb47750198ef50915679b29aff0a4d67f670d83
Scott Martin <randomcard@gmail.com>
2021-02-17 09:03:07 -0600

    Values pulled from HTML data-* come through as lowercase, updating

--- module/roll-prompt.js
+++ module/roll-prompt.js
@@ -17,26 +17,26 @@ export class RollPrompt {
 
     // Roll Formula
     // Get info, if available, defaults if not
-    let skilPart = dataset.skillValue || 0;
-    console.log(dataset.attrValue);
-    let attrPart = dataset.attrValue || 0;
+    let skilPart = dataset.skillvalue || 0;
+    console.log(dataset.attrvalue);
+    let attrPart = dataset.attrvalue || 0;
     console.log(attrPart);
     let dicePart = skilPart+attrPart;
     let explPart = dataset.explode || 10;
-    let succPart = dataset.successValue || 7;
+    let succPart = dataset.successvalue || 7;
     let enhaPart = dataset.enhancements || 0;
-    let nscaPart = dataset.narraScale || 1;
-    let dscaPart = dataset.dramaScale || 1;
+    let nscaPart = dataset.narrascale || 1;
+    let dscaPart = dataset.dramascale || 1;
 
     let rollFormula = `${dicePart}d10x${explPart}cs>=${succPart}`;
     console.log(rollFormula);
 //  let rollFormula = "(@attributes.{{key}}.value)d10x10cs>=8"
 
     // Roll Attribute by itself if no skill supplied
-    if (dataset.attrValue && !dataset.skillValue) {
+    if (dataset.attrvalue && !dataset.skillvalue) {
       console.log("Attrib only roll function triggered");
       let roll = new Roll(rollFormula, this.actor.data.data);
-      let label = dataset.attrName ? `Rolling ${dataset.attrName}` : '';
+      let label = dataset.attrname ? `Rolling ${dataset.attrname}` : '';
       roll.roll().toMessage({
         speaker: ChatMessage.getSpeaker({ actor: this.actor }),
         flavor: label
@@ -49,19 +49,20 @@ export class RollPrompt {
 /*
 rollData possible parameters:
 For the roll formula:
-.skillValue = Skill Value
-.attrValue = Attribute value
+Values pulled from HTML data-* come through as lowercase
+.skillvalue = Skill Value
+.attrvalue = Attribute value
 .explode = Explode Value (eg: 9-again)
-.successValue = Success Value (7 or 8)
+.successvalue = Success Value (7 or 8)
 .enhancements = Enhancement(s)
-.narraScale = Narrative Scale Difference
-.dramaScale = Dramatic Scale Difference
+.narrascale = Narrative Scale Difference
+.dramascale = Dramatic Scale Difference
 
 For the roll description
-.skillName = Skill Name
-.attrName = Attribute Name
-.attrArena = Attribute Arena
-.attrApproach = Attribute Approach
+.skillname = Skill Name
+.attrname = Attribute Name
+.attrarena = Attribute Arena
+.attrapproach = Attribute Approach
 Idea: import the top level skill / attrib date that will include all of these
 
 For much later, for spending successes:
