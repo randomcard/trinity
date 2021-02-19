6af1e6cc5134e9ccab67404eada6aabe1d7aecff
Scott Martin <randomcard@gmail.com>
2021-02-18 18:28:11 -0600

    fix return

--- module/trinity-roll-prompt.js
+++ module/trinity-roll-prompt.js
@@ -3,26 +3,30 @@ export class TrinityRollPrompt {
   static async tRollPrompt(rollParts) {
 
 // Open Dialog with received options
-//    function renderPrompt () {
-      const actor = this.actor;
-      const html = await renderTemplate("systems/trinity/templates/roll-prompt.html");
-      const d = Dialog.prompt({
-          title: "Roll Options",
-          content: html,
-          label: 'testButton',
-          callback: html => {
-              console.log(html, actor)
+//  function renderPrompt () {
+//    let newRollParts = rollParts;
+    const actor = this.actor;
+    const html = await renderTemplate("systems/trinity/templates/roll-prompt.html");
+    const d = Dialog.prompt({
+        title: "Roll Options",
+        content: html,
+        label: 'testButton',
+        callback: html => {
+            console.log(html, actor);
 /*
-              let passionName = html[0].querySelector('.newPassion').value
-              let passionValue = html[0].querySelector('.newPassionValue').value
+            let passionName = html[0].querySelector('.newPassion').value
+            let passionValue = html[0].querySelector('.newPassionValue').value
 
-              actor.update({
-                  "data.passions": [...actor.data.passions, [passionName, passionValue]]
-*/       }
-      });
+            actor.update({
+                "data.passions": [...actor.data.passions, [passionName, passionValue]]
+*/      }
+    });
 //    }
-  }
-
 // return the updated rollParts
     return rollParts;
+
+
+  }
+
+
 }
