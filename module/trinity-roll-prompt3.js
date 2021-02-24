98904aa74acab2ba8fe2e0f9b2f4326d9e0ca582
Scott Martin <randomcard@gmail.com>
2021-02-24 09:06:50 -0600

    attempt to add async / await // updates to pick-attr html

--- module/trinity-roll-prompt3.js
+++ module/trinity-roll-prompt3.js
@@ -19,7 +19,8 @@ export class RDialog extends Dialog {
         this.render(false);
         console.log("Test: Attr Roller Hook");
         console.log(this);
-        this.pickedElements = Picker.pDialog("attr", this.actor, this.pickedElements);
+        // this.pickedElements = Picker.pDialog("attr", this.actor, this.pickedElements);
+        this.pickedElements = async function() {await new Promise(resolve => (resolve(Picker.pDialog("attr", this.actor, this.pickedElements))))};
         console.log("Post Picker Dialog Call");
         //this.pickedElements = await new Promise(resolve => {resolve(Picker.pDialog("attr", this.actor, this.pickedElements))});
         this.render(true); // Is this needed?
