a1fbdfe24dda25370e4dc7a5ac4c8aad96dce464
Scott Martin <randomcard@gmail.com>
2021-03-09 16:19:49 -0600

    add logging

--- module/picker.js
+++ module/picker.js
@@ -20,11 +20,7 @@ export class Picker {
         html = await renderTemplate("systems/trinity/templates/pickers/pick-attr.html", {picked: pickedElements, actor: targetActor});
         break;
       case "skil":
-        console.log("targetActor.items");
-        console.log(targetActor.items);
         let pItems = targetActor.items.filter(f => f.type.includes("skill"));
-        console.log("pItems");
-        console.log(pItems);
         html = await renderTemplate("systems/trinity/templates/pickers/pick-skil.html", {items: pItems, actor: targetActor});
         break;
       default:
@@ -52,12 +48,17 @@ export class Picker {
                   pickedElements.attr = Object.values(targetActor.data.data.attributes).find(attribute => attribute.name === i.value) || pickedElements.attr;
                   pickedElements.skil = Object.values(targetActor.items).find(item => item._id === i.value) || pickedElements.skil;
 
+
                   /*
                   console.log("Picker, Update button, pickedElement:");
                   console.log(pickedElements);
                   */
                 }
               };
+
+              console.log("pickedElements");
+              console.log(pickedElements);
+
               // console.log(pickedElements);
               // resolve(pickedElements);
 
