055dfa248c838983e40b6286e744b13db5e12fc7
Scott Martin <randomcard@gmail.com>
2021-03-18 19:29:08 -0500

    add collapsible

--- module/actor/trinity-actor-sheet.js
+++ module/actor/trinity-actor-sheet.js
@@ -170,6 +170,20 @@ export class TrinityActorSheet extends ActorSheet {
       }
     }
     */
+    html.find('.collapsible').click(ev => {
+      var coll = document.getElementsByClassName("collapsible");
+      for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var content = this.nextElementSibling;
+          if (content.style.maxHeight){
+            content.style.maxHeight = null;
+          } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+          }
+        });
+      }
+    }
 
 
     // Add Inventory Item
