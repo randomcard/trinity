530039b3abe0a8322975cb93b2a679dd3669670b
Scott Martin <randomcard@gmail.com>
2021-10-27 20:48:20 -0500

    add log

--- module/trinity-roll2.js
+++ module/trinity-roll2.js
@@ -38,6 +38,7 @@ export async function trinityRoll(targetActor, pickedElements, event) {
   // Skill info
   if (typeof dataset.skillid !== 'undefined' && dataset.skillid !== null) {
     targetSkill = Object.values(targetActor.data.items).find(skill => skill._id === dataset.skillid);
+    console.log(targetSkill);
     pickedElements.skil.name = targetSkill.name;
     pickedElements.skil.value = targetSkill.data.value;
     console.log("Found Skill Info:");
