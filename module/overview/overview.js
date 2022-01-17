22a8827f17ab393ebd2625847977f4f1b6bc7040
Scott Martin <randomcard@gmail.com>
2022-01-17 14:03:40 -0600

    logging / testing

--- module/overview/overview.js
+++ module/overview/overview.js
@@ -144,6 +144,8 @@ console.log("Overview update (after), This:", this);
     // Change Momentum
     html.find('.momentum').change(ev => {
       // this.actor.update({ 'data.inspiration.value': ++this.actor.data.data.inspiration.value });
+      console.log("Momentum Change");
+      console.log(this);
       game.settings.set("trinity", "momentum-max", this.state.momentum.max);
       game.settings.set("trinity", "momentum-current", this.state.momentum.current);
       game.settings.set("trinity", "momentum-spent", this.state.momentum.spent);
