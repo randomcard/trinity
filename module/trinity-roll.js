e77ac48fc203e524748c36daede716922ffe788a
Scott Martin <randomcard@gmail.com>
2022-01-19 21:48:24 -0600

    remove FormDataExtended, try regular html.find

--- module/trinity-roll.js
+++ module/trinity-roll.js
@@ -167,7 +167,8 @@ export async function trinityRoll(targetActor, pickedElements, event) {
             content: `<form><label>Enter Saved Roll Name:</label><input name="rollNameInput" /></form>`,
             buttons: {
               submit: { label: "Submit", callback: (html) => {
-                const results = (new FormDataExtended(html.find("form")[0])).toObject();
+                // const results = (new FormDataExtended(html.find("form")[0])).toObject();
+                let results = html.find('rollNameInput');
                 // let uniqueRollNumber = randomID(16);
                 /*
                 targetActor.data.data.savedRolls[uniqueRollNumber] = {
@@ -179,7 +180,7 @@ export async function trinityRoll(targetActor, pickedElements, event) {
                 targetActor.data.data.savedRolls.name = results;
                 targetActor.data.data.savedRolls.elements = pickedElements;
                 */
-                console.log(uniqueRollNumber);
+                console.log(results);
                 console.log("Saved Roll:", targetActor.data.data.savedRolls);
               }}
             }
