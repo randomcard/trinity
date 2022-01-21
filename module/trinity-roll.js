81867df1cf64f014cc792f5d4d8df0b485f46188
Scott Martin <randomcard@gmail.com>
2022-01-20 21:25:47 -0600

    more getelement work

--- module/trinity-roll.js
+++ module/trinity-roll.js
@@ -126,7 +126,7 @@ export async function trinityRoll(targetActor, pickedElements, event) {
         label: "Submit",
         callback: () => {
         // const results = (new FormDataExtended(html.find("form")[0])).toObject();
-        let results = document.getElementById('saveName');
+        let results = document.getElementById('saveName').value;
         console.log("Save Roll As: ",results);
 /*
         let uniqueRollNumber = randomID(16);
