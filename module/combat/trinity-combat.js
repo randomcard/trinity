cd7427398e1530ca3e7d19953dd31f52458da175
Scott Martin <randomcard@gmail.com>
2022-01-29 20:27:08 -0600

    add chat output

--- module/combat/trinity-combat.js
+++ module/combat/trinity-combat.js
@@ -28,7 +28,6 @@ export class TrinityCombat extends Combat
           _id: id,
           initiative: 0
         });
-        console.log("COMBAT updates:", updates);
 
       } else {
 
@@ -38,33 +37,39 @@ export class TrinityCombat extends Combat
         let rollFormula = `(((${p.skil.value}+${p.attr.value})d10x>=${p.expl.value}cs>=${p.succ.value})*${p.nsca.value})+((${p.skil.value}+${p.attr.value})*0.01)`;
 
         const roll = game.trinity.TRoll.create(rollFormula, {}, {}, p.enha.value);
-        console.log("COMBAT roll: ", roll);
         await roll.evaluate({async: true});
-        // updates.push({_id: id, initiative: roll.total});
-        console.log("COMBAT roll: ", roll);
 
         updates.push({
           _id: id,
           initiative: roll.total
         });
-        console.log("COMBAT updates:", updates);
 
-        // Produce an initiative roll for the Combatant
-        /*
-      const roll = combatant.getInitiativeRoll(formula);
-      await roll.evaluate({async: true});
-      updates.push({_id: id, initiative: roll.total});
+        // Construct chat message data
+        let messageData = foundry.utils.mergeObject({
+          speaker: ChatMessage.getSpeaker({
+            actor: combatant.actor,
+            token: combatant.token,
+            alias: combatant.name
+          }),
+          flavor: game.i18n.format("COMBAT.RollsInitiative", {name: combatant.name}),
+          flags: {"core.initiativeRoll": true}
+        }, messageOptions);
+        const chatData = await roll.toMessage(messageData, {
+          create: false,
+          rollMode: combatant.hidden && (["roll", "publicroll"].includes(rollMode)) ? "gmroll" : rollMode
+        });
 
+        // Play 1 sound for the whole rolled set
+        if ( i > 0 ) chatData.sound = null;
+        messages.push(chatData);
 
-        ini = roll.total + (breaker * 0.01);
-        console.log("COMBAT ini: ", ini);
-*/
       }
     }
     if ( !updates.length ) return this;
 
     console.log("COMBAT THIS:", this);
     await this.updateEmbeddedDocuments("Combatant", updates);
+
     // Ensure the turn order remains with the same combatant
     if ( updateTurn && currentId ) {
       await this.update({turn: this.turns.findIndex(t => t.id === currentId)});
