708f2299c9b87b978feed916ae512888104b8e6f
Scott Martin <randomcard@gmail.com>
2022-02-02 20:53:41 -0600

    new roll

--- module/protos.js
+++ module/protos.js
@@ -33,3 +33,61 @@ export const pickedElementsProto = {
     value : false
   }
 };
+
+export const trinityRollData = {
+  name : "Trinity Roll",
+  // id : "",
+  get flavor() {
+    let text = "";
+    for (let d of this.dice) {
+      text += d.value + "●" + d.name + " "; // Expand this for better Flavortext
+    }
+    for (let e of this.enha) {
+      text += "+" + e.value + "e " + e.name + " "; // Expand this for better Flavortext
+    }
+    return text;
+  },
+  desc : "",
+  formula : "", // use Getter to compute this automatically
+  dice : {
+    /*
+    diceSourceID :{
+      value
+      name
+      SourceType
+      SourceItemID
+    }
+    */
+  },
+  enha : {
+    /*
+    enhaSourceID :{
+      value
+      name
+      SourceType
+      SourceItemID
+    }
+    */
+  },
+  get diceTotal() {
+    let total = 0;
+    for (let d of this.dice) {
+      total = total + d.value;
+    }
+    return total;
+  },
+  get enhaTotal() {
+    let total = 0;
+    for (let e of this.enha) {
+      total = total + e.value;
+    }
+    return total;
+  },
+  settings : {
+    expl : 10,
+    succ : 8,
+    nsca : 1, // Narrative Scale (Absolute)
+    dsca : 0, // Dramatic Scale (Difference)
+    init : false // For Compatibility
+  }
+};
