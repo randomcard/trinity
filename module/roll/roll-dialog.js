10e4ed5cbf30d5f8ba48a1d9ee132e19e7398bef
Scott Martin <randomcard@gmail.com>
2022-02-06 10:42:52 -0600

    underscore?

--- module/roll/roll-dialog.js
+++ module/roll/roll-dialog.js
@@ -124,7 +124,7 @@ export async function rollDialog(targetActor, rollData, event, force) {
           case "powers": itemList = powers; break;
         }
         console.log("itemList:", itemList);
-        this.render(true);
+        this._render(true);
         console.log("rendered");
         document.getElementById("overlay").style.display = "block";
         console.log("overlaid");
