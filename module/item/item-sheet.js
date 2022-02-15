0e2e0ca72250ad4c64951140c5e2e5c1538aff4a
Scott Martin <randomcard@gmail.com>
2022-02-14 21:14:39 -0600

    another

--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -99,7 +99,7 @@ export class TrinityItemSheet extends ItemSheet {
               console.log("liID", liID);
               console.log("ev", ev);
               //this.item.update({`data.subItems.-=${liID}`: null});
-              this.item.update({"data.subItems."+liID : null});
+              this.item.update({'data.subItems.-=${liID}' : null});
               // li.slideUp(200, () => this.render(false));
             }
           },
