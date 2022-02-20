1b6ed4f079a32e9c919abcb3d8140ea87b69ae36
Scott Martin <randomcard@gmail.com>
2022-02-20 12:54:17 -0600

    expanding new itemsheet to more types

--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -43,7 +43,7 @@ export class TrinityItemSheet extends ItemSheet {
     // return `${path}/item-sheet.html`;
 
     // Temporary direction as "omni" sheet is developed for different
-    let useTypes = ['action','stunt', 'tag', 'edge'];
+    let useTypes = ["attribute", "edge", "skill", "specialty", "path", "stunt", "gift", "trick", "contact", "bond", "action", "tag"];
     if (useTypes.indexOf(this.item.data.type) > -1) {
       return `${path}/item-sheet.html`;
     }
