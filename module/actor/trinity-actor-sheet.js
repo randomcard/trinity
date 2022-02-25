f2c5fc83f48a89acdda796625c94c981cf5c7e75
Scott Martin <randomcard@gmail.com>
2022-02-24 21:00:09 -0600

    see if the implict loop works

--- module/actor/trinity-actor-sheet.js
+++ module/actor/trinity-actor-sheet.js
@@ -357,11 +357,15 @@ export class TrinityActorSheet extends ActorSheet {
 
     html.find('.edit-button').click(ev => {
       console.log("edit-button clicked");
+      html.find('edit-area').classList.toggle("hidden");
+      html.find('edit-area').previousElementSibling.classList.toggle("hidden");
+      /*
       html.find('edit-area').each((i, editArea) => {
         console.log("edit-area", i, editArea);
         editArea.classList.toggle("hidden");
         editArea.previousElementSibling.classList.toggle("hidden");
       });
+      */
     });
 
     html.find('.collapsible').click(event => {
