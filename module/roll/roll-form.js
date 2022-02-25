ff886eae2aae50875516dec84be0c8ffda2c3d3d
Scott Martin <randomcard@gmail.com>
2022-02-24 22:21:49 -0600

    change logic to allow for attribute items to be used for enh rolls

--- module/roll/roll-form.js
+++ module/roll/roll-form.js
@@ -159,10 +159,10 @@ export class RollForm extends FormApplication {
     this.itemListType = type;
     this.itemList = [];
     for (let i of this.actor.items) {
+      if (type === "enhancement" && i.data.data.flags.isEnhancement === true) { this.itemList.push(i); continue; }
+      if (i.type === "attribute" && i.data.data.flags.isMain === true) { this.itemList.push(i); continue; }
       if (i.name === type) { this.itemList.push(i); continue; }
-      if (i.type === "attribute" && i.data.data.flags.isMain === false) { continue; }
       if (i.type === type) { this.itemList.push(i); }
-      if (type === "enhancement" && i.data.data.flags.isEnhancement === true) { this.itemList.push(i); }
     }
   }
 
