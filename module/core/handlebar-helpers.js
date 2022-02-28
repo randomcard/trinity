526fd2c2e6f059fc5263e773a0fff3b566788fb4
Scott Martin <randomcard@gmail.com>
2022-02-27 18:31:27 -0600

    add this.

--- module/core/handlebar-helpers.js
+++ module/core/handlebar-helpers.js
@@ -17,7 +17,7 @@ export function handlebarHelpers() {
 
   // Used for the HealthBox edit - in need of FIXME / TODO
   Handlebars.registerHelper('hbPathFixer', function(str) {
-    return actor.data.data.healthboxes[str].value;
+    return this.actor.data.data.healthboxes[str].value;
   });
 
   /* Not used now, fixed template
