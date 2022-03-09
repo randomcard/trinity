bb16a1512d4e5f69b305a608ec09daf9391e1929
Scott Martin <randomcard@gmail.com>
2022-03-08 21:32:42 -0600

    add logging

--- module/actor/trinity-actor-sheet.js
+++ module/actor/trinity-actor-sheet.js
@@ -735,6 +735,9 @@ export class TrinityActorSheet extends ActorSheet {
       itemData.data.flags.isComplication = true;
       itemData.data.complication = {};
       // itemData.data.complication.value = value;
+      console.log("header.dataset.healthtype", header.dataset.healthtype);
+      console.log("this.actor.data.data.health.details", this.actor.data.data.health.details);
+      console.log("find", this.actor.data.data.health.details.find(b => (b.type === header.dataset.healthtype)));
       itemData.data.complication.value = this.actor.data.data.health.details.find(b => (b.type === header.dataset.healthtype)).penalty;
 
       // pop-out new condition, bypass normal process
